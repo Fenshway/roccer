84c2689f5b95dce21020ff9bc7116236ddba770b
Ivan Rojas <irojas1@uncc.edu>
2022-11-01 20:38:02 -0400

    Adding route to settings

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.get('/')
 def index():
     return render_template('index.html')
+
+
+@app.get('/settings')
+def settings():
+    pass
