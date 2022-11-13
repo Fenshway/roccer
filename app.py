f2c6dd32ca4030c9df6cd9814ff68c66007211df
Justin Privett <Justin.Privett1994@gmail.com>
2022-11-13 17:55:21 -0500

    Designed Navbar

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.get('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/profile/settings')
+def settings():
+    return render_template('settings.html')
