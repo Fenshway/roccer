943078e746c8c4076054bbb5c072219320806be5
Hayden Kreuter <haydenkreuter@Haydens-MacBook-Air.local>
2022-11-21 13:29:34 -0500

    home page 1.0 and updates to database

---
+++ src/repositories/user_account_repository.py
@@ -0,0 +1,20 @@
+from src.models import User_account
+from app import db
+
+class UserRepository:
+    def get_all_users(self):
+        users = User_account.query.all()
+        return users
+
+    def get_user_by_id(self, user_id):
+        User_account.query.get(user_id)
+
+    def create_user(self, first_name, last_name, username, password):
+        new_user = User_account(first_name, last_name, username, password)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+user_repository_singleton = UserRepository()
+
+
